8e3cc3d58356b68853a4971540a2421b63632128
Thor Summerall <thor.summerall@gmail.com>
2020-11-22 16:21:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  rpg_inventory = {"hands" => "leather gloves", "arms" => "iron gauntlets", "chest" => "steel chestplate", "legs" => "applebottom jeans", "boots" => "boots with the fur"}
   
 end
 
